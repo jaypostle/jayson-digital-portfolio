28f5340584b79412c9553d415dbab2b091122752
jaypostle <50502972+jaypostle@users.noreply.github.com>
2023-01-19 11:02:17 -0800

    test update branch

--- src/assets/data/portfolioData.js
+++ src/assets/data/portfolioData.js
@@ -1,43 +1,78 @@
 // insert images here too
 
 const portfolioData = [
-    {
-        title: 'Moveo Movie Database',
-        id: 1,
-        subtitle: 'A movie database built in React.js using themoviedb.org API.',
-        techUsed: ['React.js', 'SCSS', 'JSX', 'Redux Toolkit', 'UX/UI Design'],
-        year: 2022,
-        image: '../../assets/images/Moveo/moveo-hero.png',
-        overview: 'An Event Conference website built with WooCommerce and a custom WordPress theme.',
-        
-    },
-    {
-        title: 'Capstone Rocky Mountain Festival',
-        id: 2,
-        subtitle: 'An Event Conference website built with WooCommerce and a custom WordPress theme.',
-        techUsed: ['WooCommerce', 'WordPress', 'SCSS', 'PHP', 'UX/UI Design'],
-        year: 2022,
-        image: '../../assets/images/Moveo/moveo-hero.png',
-        overview: 'A capstone project from the Front-end Web Development program at BCIT built with a team of 4. The fictitious festival, Rocky Mountain Fest, was created as a weekend getaway for the whole family. We created and organized all the content in an intuitive manner that was findable, sortable, and explorable. Highlight functionality includes being able to find the perfect Artist, Workshop, or Vendor for the family that uses a Javascript filter library, Isotope, to give the user the ability to sort by a number of popular filters.',
-        
-    },
-    {
-        title: 'Vulcan Business Website',
-        id: 3,
-        subtitle: 'Building a one-page pixel-perfect business website from scratch based on a provided design. ',
-        techUsed: ['HTML', 'Gulp', 'SCSS', 'Vanilla JS', 'GitHub'],
-        year: 2022,
-        image: '',
-        overview: 'Working with a team, we were provided a series of pdfs and screenshot videos showcasing a website design and functionality. Splitting up the work by sections, the goal was to create a consistent and pixel-perfect design while working with our first taste of GitHub and SCSS on a team project. Gulp was used to compile the SCSS. In our group of three, I took on the project manager and Dev Ops role to ensure our GitHub merges were clean and resolve any merge conflicts.',
-    },
-    {
-        title: 'Jayson Says JS Game',
-        id: 4,
-        subtitle: 'A Simon Says game built with vanilla JS and jQuery. The game is based on the 90’s Simon Says toy with 4 keys that flash in a sequence.',
-        techUsed: ['HTML', 'CSS', 'Vanilla JS', 'jQuery', 'Bootstrap'],
-        overview: 'In the opening screen, the user writes their name, selects their difficulty, and clicks start game. For help information, they can hit the help modal displayed in the top right. The game screen shows the game board, score board, and play/end game options. The keys will flash along a randomly generated sequence. Matching each “flash”, key for key, will allow you to win the game. As a class, we were learning jQuery at the time, so I built a lot of it using jQuery.',
-    },
-    
-]
+  {
+    title: "MetaHealth",
+    id: 1,
+    subtitle:
+      "A Full-stack React/Express.js app helping you discover the impact of food on your body. ",
+    techUsed: ["React.js", "SCSS", "Express.js", "Knex.js", "Node.js", "MySQL", "Chart.js"],
+    year: 2022,
+    image: "../../assets/images/Moveo/moveo-hero.png",
+    overview:
+      "An Event Conference website built with WooCommerce and a custom WordPress theme.",
+  },
+  //   {
+  //     title: "Moveo Movie Database",
+  //     id: 1,
+  //     subtitle: "A movie database built in React.js using themoviedb.org API.",
+  //     techUsed: ["React.js", "SCSS", "JSX", "Redux Toolkit", "UX/UI Design"],
+  //     year: 2022,
+  //     image: "../../assets/images/Moveo/moveo-hero.png",
+  //     overview:
+  //       "An Event Conference website built with WooCommerce and a custom WordPress theme.",
+  //   },
+  //   {
+  //     title: "Moveo Movie Database",
+  //     id: 1,
+  //     subtitle: "A movie database built in React.js using themoviedb.org API.",
+  //     techUsed: ["React.js", "SCSS", "JSX", "Redux Toolkit", "UX/UI Design"],
+  //     year: 2022,
+  //     image: "../../assets/images/Moveo/moveo-hero.png",
+  //     overview:
+  //       "An Event Conference website built with WooCommerce and a custom WordPress theme.",
+  //   },
+  {
+    title: "Moveo Movie Database",
+    id: 4,
+    subtitle: "A movie database built in React.js using themoviedb.org API.",
+    techUsed: ["React.js", "SCSS", "JSX", "Redux Toolkit", "UX/UI Design"],
+    year: 2022,
+    image: "../../assets/images/Moveo/moveo-hero.png",
+    overview:
+      "An Event Conference website built with WooCommerce and a custom WordPress theme.",
+  },
+  {
+    title: "Capstone Rocky Mountain Festival",
+    id: 5,
+    subtitle:
+      "An Event Conference website built with WooCommerce and a custom WordPress theme.",
+    techUsed: ["WooCommerce", "WordPress", "SCSS", "PHP", "UX/UI Design"],
+    year: 2022,
+    image: "../../assets/images/Moveo/moveo-hero.png",
+    overview:
+      "A capstone project from the Front-end Web Development program at BCIT built with a team of 4. The fictitious festival, Rocky Mountain Fest, was created as a weekend getaway for the whole family. We created and organized all the content in an intuitive manner that was findable, sortable, and explorable. Highlight functionality includes being able to find the perfect Artist, Workshop, or Vendor for the family that uses a Javascript filter library, Isotope, to give the user the ability to sort by a number of popular filters.",
+  },
+  {
+    title: "Vulcan Business Website",
+    id: 6,
+    subtitle:
+      "Building a one-page pixel-perfect business website from scratch based on a provided design. ",
+    techUsed: ["HTML", "Gulp", "SCSS", "Vanilla JS", "GitHub"],
+    year: 2022,
+    image: "",
+    overview:
+      "Working with a team, we were provided a series of pdfs and screenshot videos showcasing a website design and functionality. Splitting up the work by sections, the goal was to create a consistent and pixel-perfect design while working with our first taste of GitHub and SCSS on a team project. Gulp was used to compile the SCSS. In our group of three, I took on the project manager and Dev Ops role to ensure our GitHub merges were clean and resolve any merge conflicts.",
+  },
+  {
+    title: "Jayson Says JS Game",
+    id: 7,
+    subtitle:
+      "A Simon Says game built with vanilla JS and jQuery. The game is based on the 90’s Simon Says toy with 4 keys that flash in a sequence.",
+    techUsed: ["HTML", "CSS", "Vanilla JS", "jQuery", "Bootstrap"],
+    overview:
+      "In the opening screen, the user writes their name, selects their difficulty, and clicks start game. For help information, they can hit the help modal displayed in the top right. The game screen shows the game board, score board, and play/end game options. The keys will flash along a randomly generated sequence. Matching each “flash”, key for key, will allow you to win the game. As a class, we were learning jQuery at the time, so I built a lot of it using jQuery.",
+  },
+];
 
 export default portfolioData;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,51 +1,65 @@
-import '../styles/style.scss';
-import portfolioData from '../assets/data/portfolioData';
+import "../styles/style.scss";
+import portfolioData from "../assets/data/portfolioData";
 
-import Header from '../components/Header.js';
-import Footer from '../components/Footer.js';
+import Header from "../components/Header.js";
+import Footer from "../components/Footer.js";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useEffect } from 'react';
-import PageHome from '../Pages/PageHome';
-import PageContact from '../Pages/PageContact';
-import PageAbout from '../Pages/PageAbout';
-import PagePortfolio from '../Pages/PagePortfolio';
-import PageSinglePortfolio from '../Pages/PageSinglePortfolio';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import PageHome from "../Pages/PageHome";
+import PageContact from "../Pages/PageContact";
+import PageAbout from "../Pages/PageAbout";
+import PagePortfolio from "../Pages/PagePortfolio";
+import PageSinglePortfolio from "../Pages/PageSinglePortfolio";
 
-import PageProjectMoveo from '../Pages/Projects/PageProjectMoveo';
-import PageProjectRMF from '../Pages/Projects/PageProjectRMF';
-import PageProjectVulcan from '../Pages/Projects/PageProjectVulcan';
-import PageProjectJaysonSays from '../Pages/Projects/PageProjectJaysonSays';
+import PageProjectMoveo from "../Pages/Projects/PageProjectMoveo";
+import PageProjectRMF from "../Pages/Projects/PageProjectRMF";
+import PageProjectVulcan from "../Pages/Projects/PageProjectVulcan";
+import PageProjectJaysonSays from "../Pages/Projects/PageProjectJaysonSays";
+import PageProjectMetaHealth from "../Pages/Projects/PageProjectMetaHealth";
 
+import PageTechStack from "../Pages/PageTechStack";
+import PagePrivacyPolicy from "../Pages/PagePrivacyPolicy";
 
-import PageTechStack from '../Pages/PageTechStack';
-import PagePrivacyPolicy from '../Pages/PagePrivacyPolicy';
-
-import PageTestimonials from '../Pages/PageTestimonials';
+import PageTestimonials from "../Pages/PageTestimonials";
 
 function AppRouter() {
-
   useEffect(() => {
-    document.title = "Jayson | Portfolio"
-  }, [])
+    document.title = "Jayson | Portfolio";
+  }, []);
 
   return (
-    <BrowserRouter >
-      <div className='wrapper'>
+    <BrowserRouter>
+      <div className="wrapper">
         <div className="App-header">
           <Header />
         </div>
         <main>
           <Routes>
-            <Route path='/' element={<PageHome />}/>
-            <Route path='/portfolio' element={<PagePortfolio />}/>
+            <Route path="/" element={<PageHome />} />
+            <Route path="/portfolio" element={<PagePortfolio />} />
             {/* <Route path='/portfolio/:id' element={<PageSinglePortfolio />}/> */}
             {/*  Portfolios */}
-            <Route path='/portfolio/moveo-movie-database' element={<PageProjectMoveo />}/>
-            <Route path='/portfolio/capstone-rocky-mountain-festival' element={<PageProjectRMF />}/>
-            <Route path='/portfolio/vulcan-business-website' element={<PageProjectVulcan />}/>
-            <Route path='/portfolio/jayson-says-js-game' element={<PageProjectJaysonSays />}/>
-
+            <Route
+              path="/portfolio/moveo-movie-database"
+              element={<PageProjectMoveo />}
+            />
+            <Route
+              path="/portfolio/capstone-rocky-mountain-festival"
+              element={<PageProjectRMF />}
+            />
+            <Route
+              path="/portfolio/vulcan-business-website"
+              element={<PageProjectVulcan />}
+            />
+            <Route
+              path="/portfolio/jayson-says-js-game"
+              element={<PageProjectJaysonSays />}
+            />
+            <Route
+              path="/portfolio/metahealth"
+              element={<PageProjectMetaHealth />}
+            />
 
             {/* {portfolioData && portfolioData.map((project) => 
               <Route path={`/portfolio/${project.title.toLowerCase().split(" ").join("-")}`} element={<PageSinglePortfolio />}/>
@@ -53,12 +67,11 @@ function AppRouter() {
             )
             } */}
 
-            <Route path='/about' element={<PageAbout />}/>
-            <Route path='/techstack' element={<PageTechStack />}/>
-            <Route path='/contact' element={<PageContact />}/>
-            <Route path='/testimonials' element={<PageTestimonials />}/>
-            <Route path='/privacy-policy' element={<PagePrivacyPolicy />}/>
-
+            <Route path="/about" element={<PageAbout />} />
+            <Route path="/techstack" element={<PageTechStack />} />
+            <Route path="/contact" element={<PageContact />} />
+            <Route path="/testimonials" element={<PageTestimonials />} />
+            <Route path="/privacy-policy" element={<PagePrivacyPolicy />} />
           </Routes>
         </main>
         <Footer />
